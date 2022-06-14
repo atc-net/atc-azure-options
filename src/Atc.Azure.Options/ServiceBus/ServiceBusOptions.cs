67aa866e52af746b79a64cb0a117c08951dbd355
Christian Helle <christian.helle@outlook.com>
2022-06-14 16:11:28 +0200

    Remove Name from ServiceBusOptions

--- src/Atc.Azure.Options/ServiceBus/ServiceBusOptions.cs
+++ src/Atc.Azure.Options/ServiceBus/ServiceBusOptions.cs
@@ -2,10 +2,8 @@ namespace Atc.Azure.Options.ServiceBus;
 
 public class ServiceBusOptions
 {
-    public string Name { get; set; } = string.Empty;
-
     public string ConnectionString { get; set; } = string.Empty;
 
     public override string ToString()
-        => $"{nameof(Name)}: {Name}, {nameof(ConnectionString)}: {ConnectionString}";
+        => $"{nameof(ConnectionString)}: {ConnectionString}";
 }
