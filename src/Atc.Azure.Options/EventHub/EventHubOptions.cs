82d13d4f706d08a71d8860316bf936844a93c424
Anders Tøgersen (Delegate) <ato@delegate.dk]>
2023-01-12 15:36:43 +0100

    Added property FullyQualifiedNamespace to EventHub- and ServiceBusOptions

--- src/Atc.Azure.Options/EventHub/EventHubOptions.cs
+++ src/Atc.Azure.Options/EventHub/EventHubOptions.cs
@@ -6,6 +6,8 @@ public class EventHubOptions
 
     public string ConnectionString { get; set; } = string.Empty;
 
+    public string FullyQualifiedNamespace { get; set; } = string.Empty;
+
     public override string ToString()
-        => $"{nameof(Name)}: {Name}, {nameof(ConnectionString)}: {ConnectionString}";
+        => $"{nameof(Name)}: {Name}, {nameof(ConnectionString)}: {ConnectionString}, {nameof(FullyQualifiedNamespace)}: {FullyQualifiedNamespace}";
 }
